a7f94aaa2079e2dc69a86321e25f54f73e22a8dc
jhonatanquirama <quiramapino47@gmail.com>
2020-08-19 15:28:01 -0500

    actualizaciones

---
+++ 7.arreglos/ejercicio1/js/main.js
@@ -0,0 +1,13 @@
+const movies = [
+    'red:zero',
+    'avengers4',
+    'malefica'
+  ]
+  movies[3] = 'avengers2'
+  movies[0] = 'duro de matar4'
+  
+  function workWithMovies (){
+    
+  }
+  
+  console.log(movies)
--- 7.arreglos/ejercicio5/js/main.js
+++ 7.arreglos/ejercicio5/js/main.js
@@ -1,9 +1,9 @@
 const number = [1,2,3,4,5,6]
-let acumulador = 0
+let acumulador1 = 0
 
 for (let i = 0; i<number.length; i++) {
-  acumulador += number[i];  
+  acumulador1 += number[i];  
 }
 
-var dividir = acumulador/number.length
-console.log(dividir)
+var dividir1 = acumulador1/number.length
+console.log(dividir1)
