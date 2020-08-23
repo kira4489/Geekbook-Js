03cdf3e7340ac104c344bec9d7648cbe50d22f2a
unknown <quiramapino347@gmail.com>
2020-08-22 20:18:36 -0500

    actualizaciones

--- 11.asincronismo/ejercicio6/js/main.js
+++ 11.asincronismo/ejercicio6/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const message = document.querySelector('.message');
+const message = document.querySelector('.message1');
 
 setTimeout(() => {
-    message.classList.remove('hide');
+    message.classList.remove('.hide1');
 }, 1000 * 15);
